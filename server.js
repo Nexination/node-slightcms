d075128a3ae62d1da27063e0a7d739b02e2792ce
Kristian B <KrisZane@users.noreply.github.com>
2016-09-12 12:48:26 +0000

    [MISC] A commit to avoid data loss

--- server.js
+++ server.js
@@ -1,5 +1,46 @@
-let admin = new (require('./lib/admin').server);
-/*let database = new (require('./lib/database').client);
+"use strict";
+class SlightCms {
+  constructor() {
+    this.data = {};
+    this.lib = {};
+    
+    this.lib.fileConfig = new (require('fileunit').Filer)('config');
+    this.lib.fileCollectionTemplates = new (require('fileunit').Filer)('collection-templates');
+    
+    this.lib.fileCollectionTemplates.load((readError, fileData) => {this.loadCollectionTemplates(readError, fileData);});
+    this.lib.fileConfig.load((readError, fileData) => {this.loadConfig(readError, fileData);});
+  }
+  loadCollectionTemplates(readError, fileData) {
+    if(!readError) {
+      this.data.collectionTemplates = JSON.parse(fileData);
+    };
+  }
+  loadConfig(readError, fileData) {
+    if(!readError) {
+      this.data.config = JSON.parse(fileData);
+      
+      this.lib.adminServer = new (require('./lib/admin').server)(this.data.config);
+      this.lib.pathBuilderTool = new (require('./lib/pathbuilder').tool)(this.data.config);
+      this.lib.databaseClient = new (require('./lib/database').client)(this.data.config);
+    };
+  }
+  printout(error, result) {
+    console.log(error);
+    console.log(result);
+  }
+}
+let slightCms = new SlightCms();
+/*
+
+let admin = Object.create(this.data.collectionTemplates.admins);
+admin.name = 'BoJack';
+admin.password = 'HorseMan';
+admin.email = '';
+
+this.lib.databaseClient.executor('createAdmin', admin, this.printout)
+
+
+let database = new (require('./lib/database').client);
 database.connect('localhost:27017/strifecity', 'users');
 setTimeout(() => {
   database.insert([{a : 1}, {a : 2}, {a : 3}]);
